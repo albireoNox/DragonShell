95e0d2a5cc296f8222cc092641a486ac2805f6fd
Nicholas Wilson <nick@eikbek.net>
2021-08-12 12:08:17 -0500

    Add project files.

---
+++ main_cli/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace main_cli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
